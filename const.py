f005c5ca31bd8d5f0e35ed7cc3467ed3ce5f3e2a
gzzwozuiai <nijiahui007@126.com>
2025-02-08 14:51:49 +0800

    Create const.py

---
+++ const.py
@@ -0,0 +1,22 @@
+API_URL = "https://ailink-api.hotwater.com.cn/AiLinkService/device/invokeMethod"
+
+BASE_HEADERS = {
+    "Content-Type": "application/json;charset=UTF-8",
+    "X-Requested-With": "XMLHttpRequest",
+    "Sec-Ch-Ua": '"Chromium";v="127", "Not)A;Brand";v="99"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.89 Safari/537.36",
+    "Origin": "https://ailink-appservice-h5-prd.hotwater.com.cn",
+    "Referer": "https://ailink-appservice-h5-prd.hotwater.com.cn/",
+    "Version": "V1.0.1",
+    "Source": "Web",
+    "Accept-Language": "zh-CN",
+    "Accept": "application/json, text/plain, */*",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Priority": "u=1, i"
+}
