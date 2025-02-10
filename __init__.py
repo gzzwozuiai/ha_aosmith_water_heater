69cc32abb0e12fce7b998eda2c1d0c5d0a175b8c
gzzwozuiai <nijiahui007@126.com>
2025-02-10 14:22:17 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,51 @@
-"""The AO Smith Water Heater integration."""
 import logging
-
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_NAME, CONF_USERNAME, CONF_PASSWORD
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from .const import (
+    DOMAIN,
+    CONF_USERID,
+    CONF_ACCESSTOKEN,
+    CONF_FAMILYID,
+    DEFAULT_NAME
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = "aosmith_water_heater"
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_USERID): cv.string,
+                vol.Required(CONF_ACCESSTOKEN): cv.string,
+                vol.Required(CONF_FAMILYID): cv.string,
+                vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
 
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the AO Smith Water Heater component."""
-    hass.data[DOMAIN] = {}
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Ao Smith Water Heater component."""
+    conf = config[DOMAIN]
+    userid = conf.get(CONF_USERID)
+    accesstoken = conf.get(CONF_ACCESSTOKEN)
+    familyid = conf.get(CONF_FAMILYID)
+    name = conf.get(CONF_NAME)
+
+    hass.data[DOMAIN] = {
+        "userid": userid,
+        "accesstoken": accesstoken,
+        "familyid": familyid,
+        "name": name,
+    }
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config, "climate")
+    )
+
     return True
