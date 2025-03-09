cb52d2b8ecd658d75b1c6c1e89811dae18292abc
gzzwozuiai <nijiahui007@126.com>
2025-03-09 16:32:13 +0800

    Create const.py

---
+++ custom_components/hotwater_switch/const.py
@@ -0,0 +1,10 @@
+"""常量定义"""
+DOMAIN = "hotwater_switch"
+CONF_ACCESS_TOKEN = "access_token"
+CONF_USER_ID = "user_id"
+CONF_FAMILY_ID = "family_id"
+
+DEFAULT_NAME = "HotWater Heater"
+DEFAULT_DEVICE_ID = "849DC2A2714E"
+DEFAULT_PRODUCT_TYPE = "21"
+DEFAULT_DEVICE_TYPE = "DR1600HF1"
